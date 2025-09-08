874d480cca46dae4f549f75fc381387a4ab79aae
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2025-09-08 14:00:49 -0300

    começo

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('Frontend'));
+
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!'); 
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`); 
+});
